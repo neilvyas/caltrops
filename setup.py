f1ee332eecd6ee7b604c723d5b93c137be7dbb9c
Neil Vyas <vyas.neil.nv@gmail.com>
2016-11-17 21:10:09 -0800

    more buzzwords in description.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='Caltrops',
     packages=['caltrops'],
     version='0.1',
-    description='Contract-guarded computation graphs.',
+    description='Contract-guarded computation graphs for defensive programming.',
     author='Neil Vyas',
     url='',
     download_url='',
